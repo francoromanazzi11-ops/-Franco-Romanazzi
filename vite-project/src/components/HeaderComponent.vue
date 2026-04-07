d94a8e630d2069f5682bcdc051620b9bece7302d
maximiliano <santamariamaximiliano7@gmail.com>
2026-04-07 14:23:59 -0300

    clase4

---
+++ vite-project/src/components/HeaderComponent.vue
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+
+<template>
+
+<header>
+    <div>
+        <a href="#" aria-label="Patitas y Croquetas, inicio">
+            <span aria-hidden="true">🐕</span>
+            <span>Patitas y Croquetas</span>
+            <span>-Alimento para perros</span>
+        </a>
+        <nav>
+            <ul>
+                <li>Aca va a estar el nav</li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+</template>
+
+
+<style>
+
+</style>
